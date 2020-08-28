83ce7413fa503097b0f0c976384691c9d3d6f19b
avyrie <pittman.ally@gmail.com>
2020-08-28 09:03:30 -0600

    deployment

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/trailstamp';
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-})
+} || 'mongodb://localhost:27017/trailstamp')
     .then(() => console.log(`MongoDB connected successfully.`))
     .catch((err) => console.log(`MongoDB connection error: ${err}`));
 
