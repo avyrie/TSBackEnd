0827ff7046e606947752414a3689dc2bce68c1fa
avyrie <pittman.ally@gmail.com>
2020-09-11 18:25:24 -0600

    heroku

--- models/index.js
+++ models/index.js
@@ -1,16 +1,33 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/trailstamp';
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-} || 'mongodb://localhost:27017/trailstamp')
-    .then(() => console.log(`MongoDB connected successfully.`))
+})
+    .then(() => console.log(`MongoDB connected successfully. MONGO DB = `, process.env.MONGODB_URI))
     .catch((err) => console.log(`MongoDB connection error: ${err}`));
 
 module.exports = {
     Hike: require('./Hike'),
     User: require('./User'),
 };
+
+// const mongoose = require('mongoose');
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/trailstamp';
+
+// mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+// } || 'mongodb://localhost:27017/trailstamp')
+//     .then(() => console.log(`MongoDB connected successfully.`))
+//     .catch((err) => console.log(`MongoDB connection error: ${err}`));
+
+// module.exports = {
+//     Hike: require('./Hike'),
+//     User: require('./User'),
+// };
